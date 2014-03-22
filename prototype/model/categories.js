5c0ec851f43953bee78e4b6617161687fb652267
Tim Lee <bionic.tim@gmail.com>
2014-03-22 04:00:01 -0700

    Removed small creatures subcategory.

--- prototype/model/categories.js
+++ prototype/model/categories.js
@@ -2,7 +2,7 @@
 App.Model.Categories = App.Class.define(function () {
     this["paranormal"] = ["ghost & spirits", "haunted house", "possession & demons", "witches & occult & religion", "supernatural power"];
     this["killer"] = ["slasher", "crime & giallo", "redneck", "home invasion & survival", "evil child"];
-    this["monster"] = ["zombies", "vampire", "werewolf", "classic & mythological", "sci-fi & aliens", "other creatures", "animals & nature", "small creatures"];
+    this["monster"] = ["zombies", "vampire", "werewolf", "classic & mythological", "sci-fi & aliens", "other creatures", "animals & nature"];
     this["psychological"] = ["phobia", "madness & paranoia", "home invasion & survival", "surreal & psychedelic & weird", "disaster & apocalypse"];
     this["gore & disturbing"] = ["splatter", "torture", "extreme"];
 });
