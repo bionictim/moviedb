2ebf65c593ba39c510fdccaddc98c81ddfaa4feb
Tim Lee <bionic.tim@gmail.com>
2014-03-23 02:38:38 -0700

    Added psychotic subcategory

--- prototype/model/categories.js
+++ prototype/model/categories.js
@@ -1,7 +1,7 @@
 ﻿
 App.Model.Categories = App.Class.define(function () {
     this["paranormal"] = ["ghost & spirits", "haunted house", "possession & demons", "witches & occult & religion", "supernatural power"];
-    this["killer"] = ["slasher", "crime & giallo", "redneck", "home invasion & survival", "evil child"];
+    this["killer"] = ["slasher", "psychotic", "crime & giallo", "redneck", "home invasion & survival", "evil child"];
     this["monster"] = ["zombies", "vampire", "werewolf", "classic & mythological", "sci-fi & aliens", "other creatures", "animals & nature"];
     this["psychological"] = ["phobia", "madness & paranoia", "home invasion & survival", "surreal & psychedelic & weird", "disaster & apocalypse"];
     this["gore & disturbing"] = ["splatter", "torture", "extreme"];
