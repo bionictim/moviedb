ccfd5e513396cae40b7a049225ddb9a11e1ff4b0
Tim Lee <bionic.tim@gmail.com>
2014-03-23 02:57:26 -0700

    Removed phobia

--- prototype/model/categories.js
+++ prototype/model/categories.js
@@ -3,6 +3,6 @@ App.Model.Categories = App.Class.define(function () {
     this["paranormal"] = ["ghost & spirits", "haunted house", "possession & demons", "witches & occult & religion", "supernatural power"];
     this["killer"] = ["slasher", "psychotic", "crime & giallo", "redneck", "home invasion & survival", "evil child"];
     this["monster"] = ["zombies", "vampire", "werewolf", "classic & mythological", "sci-fi & aliens", "other creatures", "animals & nature"];
-    this["psychological"] = ["phobia", "madness & paranoia", "home invasion & survival", "surreal & psychedelic & weird", "disaster & apocalypse"];
+    this["psychological"] = ["madness & paranoia", "home invasion & survival", "surreal & psychedelic & weird", "disaster & apocalypse"];
     this["gore & disturbing"] = ["splatter", "torture", "extreme"];
 });
